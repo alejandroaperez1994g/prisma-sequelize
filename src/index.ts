73cac21d2054511b64ad89fa639feaede46e4931
Alejandro <alejandroaperez1994g@gmail.com>
2023-05-24 12:16:24 +0200

    :recycle: integrated swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import app from './server';
+import swaggerDocs from "./utils/swagger";
 
+const PORT = process.env.PORT || 8080;
 
-app.listen(8080, ()=>{
+app.listen(PORT, () => {
     console.log("Server is running")
+    swaggerDocs(app, Number(PORT))
 })
