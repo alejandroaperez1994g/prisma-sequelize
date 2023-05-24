c4b348847f25e5d06146faaa309c414412441b6a
Alejandro <alejandroaperez1994g@gmail.com>
2023-05-24 14:16:02 +0200

    feat: :fire: remove prisma files

--- prisma/migrations/20230523080647_remove_user_playlist/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `user` on the `Playlist` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Playlist" DROP COLUMN "user";
