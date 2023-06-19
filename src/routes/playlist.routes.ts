ea7640f8fadafc004a0ed2212cc1233210860014
Alejandro <alejandroaperez1994g@gmail.com>
2023-06-19 14:05:46 +0200

    :recycle: updated routes

--- src/routes/playlist.routes.ts
+++ src/routes/playlist.routes.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import {createPlaylist, deletePlaylist, getPlaylists} from "../controllers/playlists.controller";
+import {createPlaylist, deletePlaylist, getPlaylists, updatePlaylist} from "../controllers/playlists.controller";
 
 const playlistRouter = Router();
 
 playlistRouter
     .get("/", getPlaylists)
     .post("/:userId", createPlaylist)
+    .put("/:playlistId", updatePlaylist)
     .delete("/:playlistId", deletePlaylist)
 
 
--- src/routes/track.routes.ts
+++ src/routes/track.routes.ts
@@ -1,12 +1,11 @@
 import {Router} from 'express';
-import {addTrackToAlbum, createTrack, deleteTrack, getTracks} from "../controllers/tracks.controller";
+import {createTrack, deleteTrack, getTracks} from "../controllers/tracks.controller";
 
 
 const tracksRoutes = Router();
 
 tracksRoutes
-    .post("/add", createTrack)
-    .patch("/toAlbum/:trackId", addTrackToAlbum)
+    .post("/add/:albumId", createTrack)
     .get("/", getTracks)
     .delete("/:trackId", deleteTrack)
 
