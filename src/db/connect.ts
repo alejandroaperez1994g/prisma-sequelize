c09db63f96eafafa7d535de4d5be0222d25f0a6d
Alejandro <alejandroaperez1994g@gmail.com>
2023-06-19 14:07:36 +0200

    :recycle: updated db connection, added db synchronization

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -5,11 +5,19 @@ dotenv.config()
 
 const databaseURL = process.env.DATABASE_URL_SEQUELIZE || 'postgres://postgres2:secret@localhost:5432/postgres'
 
-export const sequelize = new Sequelize(databaseURL)
+export const sequelize = new Sequelize(databaseURL, {
+    logging: true
+})
 
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
+
+        sequelize.sync({alter: true}).then(() => {
+            console.log("Tables Updated")
+        }).then((err) => {
+            console.log(err)
+        })
         console.log('Connection has been established successfully.');
     } catch (err) {
         console.error('Unable to connect to the database:', err);
