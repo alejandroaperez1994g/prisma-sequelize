060136bb277687f514cac2d522a4700b7b7512f8
Alejandro <alejandroaperez1994g@gmail.com>
2023-06-19 14:08:41 +0200

    :sparkles: added new routes

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,14 @@ import helmet from "helmet";
 import morgan from "morgan";
 import * as dotenv from 'dotenv'
 import userRoutes from "./routes/user.routes";
+import playlistRouter from "./routes/playlist.routes";
+import albumRoutes from "./routes/album.routes";
+import tracksRoutes from "./routes/track.routes";
+import artistRoutes from "./routes/artist.routes";
 
 
 dotenv.config()
+
 
 const app = express();
 
@@ -15,8 +20,9 @@ app.use(morgan("dev"))
 app.use(express.urlencoded({extended: false}))
 
 app.use("/user", userRoutes)
-// app.use("/track", tracksRoutes)
-// app.use("/albums", albumRoutes)
-// app.use("/playlist", playlistRouter)
+app.use("/track", tracksRoutes)
+app.use("/albums", albumRoutes)
+app.use("/playlist", playlistRouter)
+app.use("/artist", artistRoutes)
 
 export default app
